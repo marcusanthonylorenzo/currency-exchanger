73592616b9b21c1b5039ae3c817eb6ed94252327
Marcus <marcus.kaitara.lorenzo@gmail.com>
2022-04-22 11:55:23 -0700

    add all currencies and change rate based on input value

--- src/index.js
+++ src/index.js
@@ -3,49 +3,62 @@ import Exchange from "./js/exchange.js";
 
 //Country selector via Base Code
 //Convert selector via other fetch?
-//
-//new Promise handling
 //DOM output: Rate from, Rate to, Timestamp "as of";
 //error handling
 
 const convertMoney = () => {
-  //currency storage
-  const currency = {
-    primary: "USD",
-  };
-
-  let convertFrom = currency.primary;
-
-  //selectors
   const button = document.getElementById("getRateBtn");
-
-  //event handling
   button.addEventListener("click", () => {
+    resetResults();
+    //get select option value
+    let input = document.getElementById("selectCountry");
+    const currency = {
+      primary: "USD",
+      target: input.options[input.selectedIndex].value,
+    };
+    console.log(currency.primary, currency.target);
+    let convertFrom = currency.primary;
+
     Exchange.getRate(convertFrom)
       .then((response) => {
-        console.table(response);
-        // Code = Rate;
         let baseCurrencyCode = response["base_code"];
-        let targetCurrencyCode = "AUD";
+        let targetCurrencyCode = currency.target;
 
         // let targetSelector = converted[responseKeys][targetCode];
-        let conversionRate = response["conversion_rates"][targetCurrencyCode];
+        let conversionRate = response["conversion_rates"][currency.target];
+        console.log(conversionRate);
 
         //async use for fetched data
-        displayResults(conversionRate);
+        displayResults(baseCurrencyCode, conversionRate, targetCurrencyCode);
         console.log(`from 1 ${baseCurrencyCode} to ${conversionRate} ${targetCurrencyCode}.`);
       });
-
   });
 };
 
-const displayResults = (data) => {
+const displayResults = (baseCode, targetRate, targetCode) => {
   let output = document.querySelector(".output");
-  let h1 = document.createElement("h1");
-  let h4 = document.createElement("h4");
-  output.append(h1, h4);
+  let baseH1 = document.createElement("h1");
+  let baseH4 = document.createElement("h4");
+  let convertedH1 = document.createElement("h1");
+  let convertedH4 = document.createElement("h4");
+  let equals = document.createElement("h3");
+  output.append(baseH1, baseH4);
+  output.append(equals);
+  output.append(convertedH1, convertedH4);
+  //placeholder value for baseH1 later.
+  baseH1.textContent = "1";
+  baseH4.textContent = baseCode;
+  equals.textContent = "is equal to";
+  convertedH1.textContent = targetRate;
+  convertedH4.textContent = targetCode;
+};
 
-  h1.textContent = data;
+const resetResults = () =>{
+  let output = document.querySelector(".output");
+  while (output.hasChildNodes()){
+    output.removeChild(output.firstElementChild);
+  }
+  return output;
 };
 
 convertMoney();
