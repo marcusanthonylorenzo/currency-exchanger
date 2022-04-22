dd59832da0f90ece8e46a8d613a4c275d6a99a46
Marcus <marcus.kaitara.lorenzo@gmail.com>
2022-04-22 10:38:23 -0700

    append to DOM on click

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import "./css/styles.css";
 import Exchange from "./js/exchange.js";
 
-//set var
-//Country selector
+//Country selector via Base Code
+//Convert selector via other fetch?
+//
 //new Promise handling
-//DOM creation
+//DOM output: Rate from, Rate to, Timestamp "as of";
 //error handling
 
 const convertMoney = () => {
+  //currency storage
   const currency = {
     primary: "USD",
-    secondary: "EUR",
   };
 
   let convertFrom = currency.primary;
@@ -18,12 +19,33 @@ const convertMoney = () => {
   //selectors
   const button = document.getElementById("getRateBtn");
 
+  //event handling
   button.addEventListener("click", () => {
     Exchange.getRate(convertFrom)
-      .then((converted) => {
-        console.table(converted);
+      .then((response) => {
+        console.table(response);
+        // Code = Rate;
+        let baseCurrencyCode = response["base_code"];
+        let targetCurrencyCode = "AUD";
+
+        // let targetSelector = converted[responseKeys][targetCode];
+        let conversionRate = response["conversion_rates"][targetCurrencyCode];
+
+        //async use for fetched data
+        displayResults(conversionRate);
+        console.log(`from 1 ${baseCurrencyCode} to ${conversionRate} ${targetCurrencyCode}.`);
       });
+
   });
+};
+
+const displayResults = (data) => {
+  let output = document.querySelector(".output");
+  let h1 = document.createElement("h1");
+  let h4 = document.createElement("h4");
+  output.append(h1, h4);
+
+  h1.textContent = data;
 };
 
 convertMoney();
